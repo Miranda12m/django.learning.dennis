18bf57ff8d84d562c4262de1d01bf3aa3dd6c841
Miranda <synysterteensier@outlook.es>
2024-08-06 23:18:49 -0600

    wasupdennis

--- users/views.py
+++ users/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Profile
+
+def loginPage(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get()
+        except:
+            messages.error(request, "User does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, "Username or Password is incorrect")
+
+    return render(request, 'users/login_register.html')
+
+def logoutPage(request):
+    logout(request)
+    messages.error(request, "User was logged out")
+    return redirect('login')
 
 def profiles(request):
     profiles = Profile.objects.all()
